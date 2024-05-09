d3d12117518d2a5afa3acf4d1454184a0dab06c0
kevinfdezdelanda <61690864+kevinfdezdelanda@users.noreply.github.com>
2024-05-09 14:56:06 +0200

    Diseño chat: mejoras diseño, autoscroll, markdown y mas

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -43,6 +43,9 @@ module.exports = {
     ],
     theme: {
         extend: {
+            boxShadow: {
+                'top-white': '0 -4px 6px -1px rgba(255, 255, 255, 0.1), 0 -2px 4px -1px rgba(255, 255, 255, 0.06)'
+            },
             fontSize:{
                 'nav': '16px',
             },
