93a105a7ca6a1c9498307a3a6c74e10571d6820f
IriarteDiamond <iriartediamond@gmail.com>
2024-05-17 13:29:35 +0200

    rag.py

--- app_sarebot/rag.py
+++ app_sarebot/rag.py
@@ -1,54 +1,72 @@
-import json
 import requests
-from sentence_transformers import SentenceTransformer
+from typing import Optional, List, Mapping, Any
 from llama_index.core import Settings, SimpleDirectoryReader, SummaryIndex, VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.llms import CustomLLM, CompletionResponse, CompletionResponseGen, LLMMetadata
+from llama_index.core.llms.callbacks import llm_completion_callback
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-class LocalEmbedding:
-    def __init__(self, model_name='all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name)
 
-    def get_embeddings(self, text_list):
-        return self.model.encode(text_list)
+API_URL = "http://172.26.215.178:1234/v1/chat/completions"
+MODEL = "bartowski/c4ai-command-r-v01-GGUF"
+TEMPERATURE = 0.4
+MAX_TOKENS = -1
 
-def llamar_api(user, system, messages=None):
-    url = "http://172.26.215.178:1234/v1/chat/completions"
-    headers = {'Content-Type': 'application/json'}
-    if messages is None:
-        messages = [
-            {"role": "system", "content": system},
-            {"role": "user", "content": user}
-        ]
-    data = {
-        "model": "bartowski/c4ai-command-r-v01-GGUF",
-        "messages": messages,
-        "temperature": 0.4,
-        "max_tokens": -1,
-        "stream": True  # Activar el streaming
-    }
-    response = requests.post(url, json=data, headers=headers, stream=True)  # Añadir 'stream=True' a la solicitud
-    if response.status_code == 200:
-        return response.iter_lines()
-    else:
-        return {'error': 'Request failed', 'status_code': response.status_code}
-def mostrar_respuesta(respuesta):
-    for chunk in respuesta:
-        if chunk:
-            data_str = chunk.decode('utf-8').strip()
-            if data_str.startswith('data: '):
-                data_str = data_str[len('data: '):]  # Eliminar 'data: ' del comienzo
-            if data_str and data_str != '[DONE]':  # Chequea si es el mensaje de finalización
-                try:
-                    data = json.loads(data_str)
-                    for choice in data.get('choices', []):
-                        delta = choice.get('delta', {})
-                        if 'content' in delta:
-                            print(delta['content'], end='')  # Usar end='' para imprimir en la misma línea
-                except json.JSONDecodeError:
-                    continue
+    
+class CustomLLM(CustomLLM):
+    context_window: int = 3900
+    num_output: int = 256
+    model_name: str = "custom"
+    api_url: str = API_URL
+    model: str = MODEL
+    temperature: float = TEMPERATURE
+    max_tokens: int = MAX_TOKENS
+    
+    @property
+    def metadata(self) -> LLMMetadata:
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    def call_api(self, user, system="Follow instructions.", messages=None, stream=False):
+        headers = {'Content-Type': 'application/json'}
+        if messages is None:
+            messages = [
+                {"role": "system", "content": system},
+                {"role": "user", "content": user}
+            ]
+        data = {
+            "model": self.model,
+            "messages": messages,
+            "temperature": self.temperature,
+            "max_tokens": self.max_tokens,
+            "stream": stream
+        }
+        response = requests.post(self.api_url, json=data, headers=headers, stream=stream)
+        if response.status_code == 200:
+            # return response.iter_lines()
+            return response.json()['choices'][0]['message']['content']
+        else:
+            raise Exception(f"Request failed with status code {response.status_code}")
+        
+    @llm_completion_callback()
+    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
+        response_text = self.call_api(prompt)
+        return CompletionResponse(text=response_text)
+    
+    @llm_completion_callback()
+    def stream_complete(self, prompt: str, **kwargs: Any) -> CompletionResponseGen:
+        response_text = self.call_api(prompt)
+        response = ""
+        for token in response_text:
+            response += token
+            yield CompletionResponse(text=response, delta=token)
+
 
 def main():
     # Carga de documentos.
@@ -60,8 +78,13 @@ def main():
     # Coge los documentos y los divide según el splitter. Se obtienen nodos.
     nodes = splitter.get_nodes_from_documents(documents)
     
+    # LLM
+    Settings.llm = CustomLLM()
+    
     # Modelo de embeddings
-    Settings.embed_model = LocalEmbedding()
+    Settings.embed_model = HuggingFaceEmbedding(
+        model_name="BAAI/bge-small-en-v1.5"
+    )
     
     # Índices
     summary_index = SummaryIndex(nodes)
@@ -78,14 +101,14 @@ def main():
     summary_tool = QueryEngineTool.from_defaults(
         query_engine=summary_query_engine,
         description=(
-            "Useful for summarization questions related to MetaGPT"
+            "Useful for summarization questions related to the 'documento' file."
         ),
     )
 
     vector_tool = QueryEngineTool.from_defaults(
         query_engine=vector_query_engine,
         description=(
-            "Useful for retrieving specific context from the MetaGPT paper."
+            "Useful for retrieving specific context from the 'documento' file."
         ),
     )
     
@@ -99,14 +122,12 @@ def main():
         verbose=True
     )
     
-    response = query_engine.query(
-        "¿Quien es el responsable del departamento de IA de SareNet?"
-    )
-    print(str(response))
-        
     # Query
-    # respuesta = llamar_api("¿Quien es el responsable del departamento de IA de SareNet?", "Answer the questions from user concisely. If you don't know a specific answer, just say it.")
-    # print(mostrar_respuesta(respuesta))
+    response = query_engine.query(
+        "¿Quiénes componen el equipo SareBot?"
+    )
+
+    print(f"\n\nResponse string: {str(response)}")
 
 if __name__ == "__main__":
     main()
