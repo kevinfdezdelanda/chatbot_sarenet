41b9cb0a27dd9ce900b8acdec12f63c9ca969e29
IriarteDiamond <iriartediamond@gmail.com>
2024-05-22 14:55:33 +0200

    Arreglo RAG
    No se cargaban bien los índices a partir de los documentos existentes al iniciar la aplicación.

--- app_sarebot/state.py
+++ app_sarebot/state.py
@@ -1,20 +1,28 @@
 import logging
-from llama_index.core import SummaryIndex, VectorStoreIndex, Settings
+from llama_index.core import SummaryIndex, VectorStoreIndex, Settings, SimpleDirectoryReader
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.selectors import LLMSingleSelector
 from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.node_parser import SentenceSplitter
 from .llm_model import llm
 
 NODES = []
 _query_engine = None
 
+def load_and_index_documents():
+    global NODES
+    documents = SimpleDirectoryReader(input_dir="data").load_data()
+    splitter = SentenceSplitter(chunk_size=1024)
+    NODES.clear()
+    NODES.extend(splitter.get_nodes_from_documents(documents))
+
 def update_query_engine():
     global _query_engine, NODES
 
-    if not NODES:
-        logging.warning("NODES is empty. Query engine will not be initialized.")
-        return
+    # if not NODES:
+    #     logging.warning("NODES is empty. Query engine will not be initialized.")
+    #     return
 
     Settings.llm = llm
     Settings.embed_model = HuggingFaceEmbedding(
